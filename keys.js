ece447d5a448d567c928521676e9b6c075eef380
joeyfboone <joey@urgentmail.com>
2017-09-24 19:23:56 -0400

    added keys.js liri.js and random.txt

---
+++ keys.js
@@ -0,0 +1,11 @@
+
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'zkwI92jXFB5ogHlGLxtOT7UyC',
+  consumer_secret: '1JnLnxCSXkiDPAw2vw6by0xqIGYlsyjxgpGDkqSln3vsTQahPw',
+  access_token_key: '16194608-4AM8bheHUona6ON9BR7oAqxWtUbbhF8zxXdACpNxR',
+  access_token_secret: 'mj7KbBoqEApLvwW8xVAbtLW7IXiRojpYTgbGmPJH1WVoS',
+}
+
+module.exports = twitterKeys;
